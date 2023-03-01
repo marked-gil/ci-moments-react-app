4c87154def9ab26663ce63ff9eeaa0050a10a345
marked-gil <markgil_creates@yahoo.com>
2023-03-01 01:08:56 +0000

    Add React-router library

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import NavBar from './components/NavBar';
-import styles from './App.module.css'
+import styles from './App.module.css';
+import { Route, Switch } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home Page</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+          <Route render={() => <h1>Page Not Found</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
