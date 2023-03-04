29c23d5cf03110284a9c56c8382149fee9ee100e
marked-gil <markgil_creates@yahoo.com>
2023-03-04 00:16:26 +0000

    Add Sign Up Form with authentication

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import NavBar from './components/NavBar';
 import styles from './App.module.css';
+import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
 import { Route, Switch } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 
--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // Unique URL for deployed API project
-axios.defaults.baseURL = 'https://dj-rest-framework-api.herokuapp.com/';
+axios.defaults.baseURL = "https://dj-rest-framework-api.herokuapp.com/";
 // Data format API expects; multipart - due to images & texts in its request
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 // To avoid CORS errors when sending cookies
 axios.defaults.withCredentials = true;
