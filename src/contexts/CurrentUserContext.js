cd0939c68f548f13d0d36362b8f242dedf574f46
marked-gil <markgil_creates@yahoo.com>
2023-03-04 23:30:44 +0000

    Adjust NavBar for loggedIn and loggedOut

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -1,7 +1,7 @@
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import axios from "axios";
 import { axiosReq, axiosRes } from "../api/axiosDefaults";
-import { useHistory } from "react-router-dom";
+import { useHistory } from "react-router";
 
 export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
@@ -53,18 +53,18 @@ export const CurrentUserProvider = ({ children }) => {
       async (err) => {
         if (err.response?.status === 401) {
           try {
-            await axios.post("/dj-rest-auth/token/refresh");
+            await axios.post("/dj-rest-auth/token/refresh/");
           } catch (err) {
             setCurrentUser((prevCurrentUser) => {
               if (prevCurrentUser) {
-                history.push('/signin')
+                history.push("/signin");
               }
-              return null
-            })
+              return null;
+            });
           }
-          return axios(err.config)
+          return axios(err.config);
         }
-        return Promise.reject(err)
+        return Promise.reject(err);
       }
     )
   }, [history]);
