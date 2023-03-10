0c7494e09b19443ad05b36d75ec12baaa36735f3
marked-gil <markgil_creates@yahoo.com>
2023-03-10 22:10:50 +0000

    Comment out console.logs

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -42,7 +42,7 @@ const Post = (props) => {
       await axiosRes.delete(`/posts/${id}/`);
       history.goBack();
     } catch (err) {
-      console.log(err);
+      // console.log(err);
     }
   };
 
@@ -58,7 +58,7 @@ const Post = (props) => {
         }),
       }));
     } catch (err) {
-      console.log(err);
+      // console.log(err);
     }
   };
 
@@ -74,7 +74,7 @@ const Post = (props) => {
         }),
       }));
     } catch (err) {
-      console.log(err);
+      // console.log(err);
     }
   };
 
